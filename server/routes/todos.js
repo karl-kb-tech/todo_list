e076a1a95b088fd6919e9077868fc4aad3b28da4
Karl Brocklehurst <kairakau@gmail.com>
2019-02-25 11:44:50 +1300

    post get and /:priority working for priority api

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -16,12 +16,20 @@ router.get('/', (req, res) => {
 
 //POST /api/v1/priority
 router.post('/', (req, res) => {
-
+  res.json('post is working')
 })
 
 //GET /api/v1/priority/:priority
 router.get('/:priority', (req, res) => {
-
+  res.json([
+    {
+      task: 'get coke',
+      priority: 1,
+      category: "food & drink",
+      is_complete: false,
+      due_at: 1551135600
+    }
+  ])
 })
 
 module.exports = router
